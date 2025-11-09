1225e3d660354b06a2b92aafd1e5393e2b9e405e
divya sharma <sddivyasharma2005@gmail.com>
2025-11-08 23:28:42 -0600

    promise function and timeout

---
+++ script.js
@@ -0,0 +1,22 @@
+// promise task
+function simulateAsyncTask() {
+	return new Promise((resolve) => {
+		setTimeout(() => resolve("Async task completed!"), 2000);
+	});
+}
+console.log("Task started...");
+simulateAsyncTask().then((msg) => console.log(msg));
+console.log("This logs before the async task finishes!");
+
+
+async function fetchCatBreeds() {
+	try {
+		const response = await fetch("https://catfact.ninja/breeds");
+		if (!response.ok) throw new Error("Network response was not ok");
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error("Error fetching breeds:", error);
+	}
+}
+fetchCatBreeds().then((data) => console.log("Breeds data:", data));
